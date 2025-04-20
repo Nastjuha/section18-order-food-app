2a1cc8ea9513d287d39af88eeefda4d9a4321966
Anastasiia Lukianets <lukianets.anastasiia@gmail.com>
2025-04-20 17:37:39 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Cart from "../src/components/Cart.tsx";
+
+function App() {
+  return (
+    <>
+      <h1>You got this 💪</h1>
+      <p>Stuck? Not sure how to proceed?</p>
+      <p>Don't worry - we've all been there. Let's build it together!</p>
+      <Cart />
+    </>
+  );
+}
+
+export default App;
